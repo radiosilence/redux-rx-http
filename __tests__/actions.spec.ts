d71c43438ed27225c9bf56f7ca3f666fe4b057f4
James Cleveland <jc@blit.cc>
2018-01-25 16:04:23 +0000

    add cases for patch delete head

--- __tests__/actions.spec.ts
+++ __tests__/actions.spec.ts
@@ -57,7 +57,6 @@ describe('rxHttpPost', () => {
     })
 })
 
-
 describe('rxHttpPut', () => {
     it('should return a put action', () => {
         const action = A.rxHttpPut(URL, ACTION_TYPES)
@@ -80,6 +79,48 @@ describe('rxHttpPut', () => {
                 url: URL,
                 method: 'PUT',
                 body: { potato: 'tomato' },
+            },
+        })
+    })
+})
+
+describe('rxHttpDelete', () => {
+    it('should return a delete action', () => {
+        const action = A.rxHttpDelete(URL, ACTION_TYPES)
+        expect(action).toMatchObject({
+            type: A.RX_HTTP_REQUEST,
+            actionTypes: ACTION_TYPES,
+            request: {
+                url: URL,
+                method: 'DELETE',
+            },
+        })
+    })
+})
+
+describe('rxHttpHead', () => {
+    it('should return a head action', () => {
+        const action = A.rxHttpHead(URL, ACTION_TYPES)
+        expect(action).toMatchObject({
+            type: A.RX_HTTP_REQUEST,
+            actionTypes: ACTION_TYPES,
+            request: {
+                url: URL,
+                method: 'HEAD',
+            },
+        })
+    })
+})
+
+describe('rxHttpPatch', () => {
+    it('should return a patch action', () => {
+        const action = A.rxHttpPatch(URL, ACTION_TYPES)
+        expect(action).toMatchObject({
+            type: A.RX_HTTP_REQUEST,
+            actionTypes: ACTION_TYPES,
+            request: {
+                url: URL,
+                method: 'PATCH',
             },
         })
     })
