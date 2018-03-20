d555dc5c5a9008fff922faceec11ec4c16bec172
Robert Tucker <robertlgtucker@gmail.com>
2018-03-20 17:23:31 +0000

    updates types

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/interfaces'
-export * from './src/actions'
-export * from './src/epics'
-export * from './src/utils'
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './actions'
+export * from './epics'
+export * from './interfaces'
+export * from './utils'
